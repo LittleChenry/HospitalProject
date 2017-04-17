857c9f9fba1f78fef113c2a7ab00d8c8bab06ffc
peach <405891205@qq.com>
2017-04-17 20:45:01 +0800

    测试取消注释

--- Root/Root-userInformation.aspx.cs
+++ Root/Root-userInformation.aspx.cs
@@ -16,8 +16,8 @@ public partial class Root_Root_userInformation : System.Web.UI.Page
         {
             if (Session["loginUser"] == null)
             {
-                //MessageBox.Message("请先登陆");
-                //Response.Write("<script language=javascript>window.location.replace('../Main/Login.aspx');</script>");
+                MessageBox.Message("请先登陆");
+                Response.Write("<script language=javascript>window.location.replace('../Main/Login.aspx');</script>");
             }
         }
         string isposback = Request.Form["ispostback"];
