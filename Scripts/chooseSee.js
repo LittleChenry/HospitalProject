e52a2678d205aa3ed710ef548c571f069b02384f
peach <405891205@qq.com>
2017-05-09 10:23:59 +0800

    修改管理员模块的一个bug

---
+++ Scripts/chooseSee.js
@@ -0,0 +1,26 @@
+﻿window.addEventListener("load", Init, false);
+
+function Init() {
+    document.getElementById('enableSee').addEventListener("click", showHide, false);
+}
+
+function showHide(evt) {
+    var hidePart = document.getElementById("hidePart");
+    var middle = document.getElementById("middleArea");
+    //ie是element.currentStyle最终css,别的事wind.getComputedStyle(element,伪元素)获取最终css,element.style只能获取当前stlye值，第二个是只读的
+    var style = (hidePart.currentStyle != undefined) ? hidePart.currentStyle.display : window.getComputedStyle(hidePart, null).display;
+    if (style == "none") {
+        hidePart.style.display = "list-item";
+        document.getElementById('enableSeeSpan').className = "fa fa-angle-double-down";
+        if (middle != undefined) {
+            middle.style.minHeight = "1800px";
+        }
+    } else {
+        hidePart.style.display = "none";
+        document.getElementById('enableSeeSpan').className = "fa fa-angle-double-left";
+        if (middle != undefined) {
+            middle.style.minHeight = "800px";
+        }
+    }
+    evt.preventDefault();
+}
